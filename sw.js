d4579b7c0bfe0f6bc48682874ce4177bbb67aeb8
SilenLoc <88315530+SilenLoc@users.noreply.github.com>
2023-03-11 20:59:44 +0000

    Deploying to gh-pages from @ SilenLoc/omnis-vanitas-web@b3d2bb4e626db23c2bb9fce63675059ef6a9e5ea 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'omni-vanity-web-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './omnis-vanitas-web.js',
+  './omnis-vanitas-web.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
